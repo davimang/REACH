d3e70382fb48beab1ef05439ec24383172bc25c4
David Moroniti <davidmor956@gmail.com>
2023-11-05 14:40:19 -0500

    Add base setup for an api endpoint + simple jwt authentication

--- src/backend/reach_backend/Api/views.py
+++ src/backend/reach_backend/Api/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import User, Group
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework import viewsets
 from rest_framework import permissions
 from Api.serializers import UserSerializer, GroupSerializer
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -20,3 +23,15 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+#temporary endpoint for POC
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def fetch_trials(request):
+    query_params = request.query_params
+    #use for trial api request
+    age = query_params.get("age")
+    gender = query_params.get("gender")
+    return Response({"age": age, "gender": gender})
