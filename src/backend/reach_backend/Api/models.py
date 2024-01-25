48da2886be6f2ef4ac4a7eb32e41ef13038a2a33
David Moroniti <davidmor956@gmail.com>
2024-01-25 11:52:33 -0500

    Add user data, patient info, trial models + serializers

--- src/backend/reach_backend/Api/models.py
+++ src/backend/reach_backend/Api/models.py
@@ -1,1 +1,36 @@
 """Module defining the models for the Api service."""
+from django.db import models
+
+class UserData(models.Model):
+    """User data model."""
+    created = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    is_clinician = models.BooleanField()
+
+    class Meta:
+        ordering = ['created']
+
+
+class PatientInfo(models.Model):
+    """Patient info model."""
+    class Gender:
+        MALE = "M", _("Male")
+        FEMALE = "F", _("Female")
+        OTHER = "O", _("Other")
+
+    user = models.ForeignKey(UserData)
+    date_of_birth = models.DateField()
+    #address = ?
+    gender = models.CharField(
+        max_length=1,
+        choices=Gender
+    )
+    advanced_info = models.JSONField()
+
+class Trial(models.Model):
+    """Trial model."""
+    user = models.ForeignKey(UserData)
+    title = models.TextField()
+    description = models.TextField()
+    url = models.URLField(max_length=200)
--- src/backend/reach_backend/Api/serializers.py
+++ src/backend/reach_backend/Api/serializers.py
@@ -1,6 +1,7 @@
 """Module defining the serializers."""
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from models import UserData, PatientInfo, Trial
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -19,3 +20,24 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         """Meta class for group serializer."""
         model = Group
         fields = ["url", "name"]
+
+
+class UserDataSerializer(serializers.ModelSerializer):
+    """Serializer for the user data model."""
+    class Meta:
+        model = UserData
+        fields = ['first_name', 'last_name', 'is_clinician']
+
+
+class PatientInfoSerializer(serializers.ModelSerializer):
+    """Serializer for the patient info model."""
+    class Meta:
+        model = PatientInfo
+        fields = ['date_of_birth', 'gender', 'advanced_info']
+
+
+class TrialSerializer(serializers.ModelSerializer):
+    """Serializer for the trial model."""
+    class Meta:
+        model = Trial
+        fields = ['title', 'description', 'url']
