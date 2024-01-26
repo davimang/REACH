ae31e15fb9cb9a00df39aa6761784464b11a7daf
Alan Scott <alansco.scott@gmail.com>
2024-01-25 19:51:59 -0500

    refactored a little bit

--- src/python/TrialFilterer.py
+++ src/python/TrialFilterer.py
@@ -23,21 +23,32 @@ class TrialFilterer:
         studies['MaximumAge'] = studies['MaximumAge'].apply(lambda x : TrialFilterer.clean_age(x))
         studies = studies[(age >= studies["MinimumAge"]) & (age <= studies["MaximumAge"])]
 
-        #create address, calculate geodesic distance
-        studies['FullAddress'] = ""
-        for i in studies.index:
-            studies.at[i, 'FullAddress'] = studies.at[i, 'LocationCity'] if not pd.isnull(studies.at[i, 'LocationCity']) else studies.at[i, 'FullAddress']
-            studies.at[i, 'FullAddress'] = studies.at[i, 'FullAddress'] + ", " + studies.at[i, 'LocationState'] if not pd.isnull(studies.at[i, 'LocationState']) else studies.at[i, 'FullAddress']
-            studies.at[i, 'FullAddress'] = studies.at[i, 'FullAddress'] + " " + studies.at[i, 'LocationZip'] if not pd.isnull(studies.at[i, 'LocationZip']) else studies.at[i, 'FullAddress']
-            studies.at[i, 'FullAddress'] = studies.at[i, 'FullAddress'] + ", " + studies.at[i, 'LocationCountry'] if not pd.isnull(studies.at[i, 'LocationCountry']) else studies.at[i, 'FullAddress']
+        #calculate geodesic distance
         studies['Distance'] = studies['FullAddress'].apply(lambda x : TrialFilterer.get_distance_km(home_address, x))
-
 
         studies = TrialFilterer.filter_gender(input_params['sex'], studies)
 
         studies.sort_values('Distance', inplace=True)
 
         return studies
+    
+    @staticmethod
+    def filter_keywords(
+        df: pd.DataFrame
+    ) -> pd.DataFrame:
+        pass
+    
+    @staticmethod
+    def generate_address(
+        studies: pd.DataFrame
+    ) -> pd.DataFrame:
+        '''generates the address of the trial through the given fields'''
+        studies['FullAddress'] = ""
+        for i in studies.index:
+            studies.at[i, 'FullAddress'] = studies.at[i, 'LocationCity'] if not pd.isnull(studies.at[i, 'LocationCity']) else studies.at[i, 'FullAddress']
+            studies.at[i, 'FullAddress'] = studies.at[i, 'FullAddress'] + ", " + studies.at[i, 'LocationState'] if not pd.isnull(studies.at[i, 'LocationState']) else studies.at[i, 'FullAddress']
+            studies.at[i, 'FullAddress'] = studies.at[i, 'FullAddress'] + " " + studies.at[i, 'LocationZip'] if not pd.isnull(studies.at[i, 'LocationZip']) else studies.at[i, 'FullAddress']
+            studies.at[i, 'FullAddress'] = studies.at[i, 'FullAddress'] + ", " + studies.at[i, 'LocationCountry'] if not pd.isnull(studies.at[i, 'LocationCountry']) else studies.at[i, 'FullAddress']
     
     @staticmethod
     def filter_gender(
