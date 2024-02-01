3dc226d76525d91860b30b9d9933564c0b70eb61
aami-hus <77757839+aami-hus@users.noreply.github.com>
2024-01-31 21:11:31 -0500

    I112 implement react router to manage webpage state (#113)
    * added temporary webpage state management solution
    
    * replace main class component with functional component
    
    * added react-router-dom dependency, replaced state management with react router

---
+++ src/FE/FEsrc/components/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+
+const LoginPage = () => {
+    
+    return (
+        <div>LOGIN PAGE</div>
+    );
+}
+
+export default LoginPage;
