2292d076bb3d0f4dafdeba394d9be672ae982d0e
aami-hus <hussaa54@mcmaster.ca>
2024-01-31 20:00:43 -0500

    replace main class component with functional component

--- src/FE/FEsrc/components/App.tsx
+++ src/FE/FEsrc/components/App.tsx
@@ -2,31 +2,13 @@ import React, { Component } from 'react';
 import LandingPage from './pages/LandingPage';
 import MenuHeader from './MenuHeader';
 
-interface AppProps {
-    message: string;
-}
-
-interface AppState {
-    rendered: boolean;
-}
-
-class App extends Component<AppProps, AppState> {
-    constructor(props: AppProps) {
-        super(props);
-        this.state = {
-            rendered: false
-        }
-    }
-
-    render() {
+function App () {
+    
         return (
             <>
-                {/* <h1>{this.props.message}</h1> */}
                 <MenuHeader />
             </>
         );
-    }
-
 }
 
 export default App;
