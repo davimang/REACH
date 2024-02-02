bfae23718bc0db64a7968b6834431a54b8c36d11
Deep123845 <deep123845@gmail.com>
2024-02-02 11:23:43 -0500

    added missing slash to register url

--- src/backend/reach_backend/Api/urls.py
+++ src/backend/reach_backend/Api/urls.py
@@ -15,7 +15,7 @@ from .views import (
 
 urlpatterns = [
     path("search_trials/", search_trials, name="search-trials"),
-    path("register", UserRegistrationView.as_view(), name="user-registration"),
+    path("register/", UserRegistrationView.as_view(), name="user-registration"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
