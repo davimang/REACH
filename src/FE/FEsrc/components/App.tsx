11608156a60c24958456005540a019040064a885
Deep Raj <51278594+deep123845@users.noreply.github.com>
2024-02-03 13:02:52 -0500

    Frontend authentication (#119)
    * set simple_jwt lifetime
    
    * added login endpoint
    
    * refomatted settings
    
    * added registration serializers
    
    * added registration endpoint, removed login endpoint because of redundancy
    
    * added refresh token to response
    
    * unfinished draft
    
    * first draft of login page and authcontext
    
    * added userid to context
    
    * added refreshing access token
    
    * draft of registration
    
    * fixed registration
    
    * added register button to login page

--- src/FE/FEsrc/components/App.tsx
+++ src/FE/FEsrc/components/App.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import { AuthProvider } from '../contexts/AuthContext';
+
 import MenuHeader from './MenuHeader';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
-function App () {
+
+function App() {
 
     return (
-        <BrowserRouter>
-            <MenuHeader />
-            <Routes>
-                <Route path='/' element={<LandingPage />} />
-                <Route path='/login' element={<LoginPage />} />
-            </Routes>
-        </BrowserRouter>
+        <AuthProvider>
+            <BrowserRouter>
+                <MenuHeader />
+                <Routes>
+                    <Route path='/' element={<LandingPage />} />
+                    <Route path='/login' element={<LoginPage />} />
+                    <Route path='/register' element={<RegisterPage />} />
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
     );
 }
 
