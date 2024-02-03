4d72c97ce9d4cf8658fa81e6121b2d26a4651962
Deep123845 <deep123845@gmail.com>
2024-02-03 01:57:52 -0500

    added refreshing access token

--- src/FE/FEsrc/contexts/AuthContext.tsx
+++ src/FE/FEsrc/contexts/AuthContext.tsx
@@ -1,5 +1,4 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 import { jwtDecode } from 'jwt-decode';
 import { API_URL } from '..';
 
@@ -61,6 +60,69 @@ export const AuthProvider: React.FC<AuthContextProps> = ({ children }) => {
         setUserId(null);
         setAuthenticated(false);
     };
+
+    const refreshAccessToken = async () => {
+        try {
+            const refreshToken = localStorage.getItem('refreshToken');
+
+            if (!refreshToken) {
+                throw new Error('Refresh token not found');
+            }
+
+            const response = await fetch(`${API_URL}/token/refresh/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ refresh: refreshToken }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Token refresh failed');
+            }
+
+            const { access } = await response.json();
+
+            localStorage.setItem('accessToken', access);
+            setAuthenticated(true);
+
+        } catch (error) {
+            console.error('Token refresh failed:', error);
+            logout();
+        }
+    };
+
+    useEffect(() => {
+        const checkTokenExpiration = () => {
+            const accessToken = localStorage.getItem('accessToken');
+
+            if (accessToken) {
+                const decodedToken: any = jwtDecode(accessToken);
+                const timeToExpiration = decodedToken.exp * 1000 - Date.now();
+
+                const refreshTime = 65000;
+
+                if (timeToExpiration < refreshTime && timeToExpiration > 0) {
+                    refreshAccessToken();
+                }
+
+                if (timeToExpiration > 0) {
+                    setAuthenticated(true);
+                }
+                else {
+                    refreshAccessToken();
+                }
+            }
+        };
+
+        checkTokenExpiration();
+
+        const tokenCheckInterval = setInterval(() => {
+            checkTokenExpiration();
+        }, 60000);
+
+        return () => clearInterval(tokenCheckInterval);
+    }, []);
 
     return (
         <AuthContext.Provider value={{ isAuthenticated, userId, login, logout }}>
