c01f1af73dc6dba6b4fffcc1949757ffcc268026
David Moroniti <davidmor956@gmail.com>
2024-02-03 14:27:18 -0500

    Add contact us + about us pages

---
+++ src/FE/FEsrc/components/pages/AboutUsPage.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const AboutUsContainer = styled.div`
+    display: inline-flex;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const AboutUsText = styled.div`
+    padding: 25px;
+    width: 45vw;
+    color: #FFFFFF;
+    font-size: 20px;
+    font-family: math;
+`;
+
+const AboutUsPage = () => {
+    
+    return (
+        <AboutUsContainer>
+            <AboutUsText>
+                <h1>Motivation behind REACH</h1>
+                <p>Reach was developed by a team of final year Software Engineering students
+                    as part of an eight month long capstone course. Additionally, the project was supervised (and proposed) by 
+                    two respirolagists from Mcmaster, Dr. Terrence Ho & Dr. Ciaran Scallan. It was created
+                    with the ultimate goal of providing both clincians & patients easier access to clincial trials from 
+                    around the world.
+                </p>
+                <h1>The Engineering Team</h1>
+                <p>The team consists of Aamina Hussain, Alan Scott, Anika Peer, David Moroniti, and Deep Raj. If you would
+                    like to contact one of the members, please see our "Contact Us" page.
+                </p>  
+            </AboutUsText>
+        </AboutUsContainer>
+    );
+}
+
+export default AboutUsPage;
