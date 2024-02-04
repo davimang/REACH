fe5bbcca08cd95eb1836abf2b9722fce18a94211
A Peer <47980558+AKP101@users.noreply.github.com>
2024-02-04 16:07:57 -0500

    Prelim imports and react cahnges

--- src/FE/FEsrc/components/UserProfileCard.tsx
+++ src/FE/FEsrc/components/UserProfileCard.tsx
@@ -7,18 +7,16 @@ interface UserProfileCardProps{
 const UserProfileCard: React.FC<UserProfileCardProps> = ({name}) => {
     return (
         <div className="cards">
-            <div className="namecircle">
-                <span>{name.charAt(0)} {name.split(" ").pop().charAt(0)}</span>
+            <div className="name-circle">
+                <span>{name.charAt(0)}</span>
             </div>
             <div className="card-content">    
-                <h1>{name}</h1>
+                <h1 className="profile-name" >{name}</h1>
+                <a href="saved-trials.html" className="saved-trials-link">See saved trials</a>
             </div>
         </div>
     );
 }
-
-
-
 
 
 export default UserProfileCard;
