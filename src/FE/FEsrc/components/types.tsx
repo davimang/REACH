45d9cbae5ad77b7e02a34b063133571a210c9128
aami-hus <77757839+aami-hus@users.noreply.github.com>
2024-02-03 19:50:59 -0500

    I107 trial search page (#126)
    * draft of trial search page
    
    * map response data to components, added required states and types

--- src/FE/FEsrc/components/types.tsx
+++ src/FE/FEsrc/components/types.tsx
@@ -17,3 +17,40 @@ export interface ClinicalTrial {
 export interface ClinicalTrialsData {
     [key: string]: ClinicalTrial;
 }
+
+export interface Address {
+    street: string;
+    city: string;
+    postalCode: string;
+    province: string;
+}
+
+export interface PatientInfo {
+    id: number;
+    date_of_birth: string;
+    address: Address;
+    gender: string;
+    advanced_info: number; // TODO: will be dict of advanced info - must update this type later
+    title: string;
+    condition: string;
+    user: number;
+}
+
+export interface PatientInfoList {
+    [key: string]: PatientInfo;
+}
+
+export interface TrialInfo {
+    NCTId: string;
+    BriefTitle: string;
+    DetailedDescription: string;
+    OverallStatus: string;
+    Distance: number;
+    KeywordRank: number;
+    url: string;
+}
+
+export interface TrialInfoList {
+    [key: string]: TrialInfo;
+}
+  
