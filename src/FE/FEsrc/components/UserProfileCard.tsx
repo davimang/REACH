91c9a62fd32cac0af953b43e8a8ca0390d7eaad4
A Peer <47980558+AKP101@users.noreply.github.com>
2024-02-04 22:45:54 -0500

    updates

--- src/FE/FEsrc/components/UserProfileCard.tsx
+++ src/FE/FEsrc/components/UserProfileCard.tsx
@@ -1,22 +1,22 @@
-import React from "react";
+// import React from "react";
 
-interface UserProfileCard{
-    name: string;
-}
+// interface UserProfileCard{
+//     name: string;
+// }
 
-const UserProfileCard: React.FC<UserProfileCard> = ({name}) => {
-    return (
-        <div className="cards">
-            <div className="name-circle">
-                <span>{name.charAt(0)}</span>
-            </div>
-            <div className="card-content">    
-                <h1 className="profile-name" >{name}</h1>
-                <a href="saved-trials.html" className="saved-trials-link">See saved trials</a>
-            </div>
-        </div>
-    );
-}
+// const UserProfileCard: React.FC<UserProfileCard> = ({name}) => {
+//     return (
+//         <div className="cards">
+//             <div className="name-circle">
+//                 <span>{name.charAt(0)}</span>
+//             </div>
+//             <div className="card-content">    
+//                 <h1 className="profile-name" >{name}</h1>
+//                 <a href="saved-trials.html" className="saved-trials-link">See saved trials</a>
+//             </div>
+//         </div>
+//     );
+// }
 
 
-export default UserProfileCard;
+// export default UserProfileCard;
