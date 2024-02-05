40bf1ad5c02840f368869619146f7a4a6bc96a6c
David Moroniti <davidmor956@gmail.com>
2024-02-05 11:00:03 -0500

    Uncomment routes

--- src/FE/FEsrc/components/App.tsx
+++ src/FE/FEsrc/components/App.tsx
@@ -19,12 +19,12 @@ function App() {
             <BrowserRouter>
                 <MenuHeader />
                 <Routes>
-                    {/* <Route path='/' element={<LandingPage />} />
+                    <Route path='/' element={<LandingPage />} />
                     <Route path='/login' element={<LoginPage />} />
                     <Route path='/register' element={<RegisterPage />} />
                     <Route path='/about' element={<AboutUsPage/>} />
                     <Route path='/contact' element={<ContactUsPage/>}/>
-                    <Route path='/faq' element={<FAQPage/>}/> */}
+                    <Route path='/faq' element={<FAQPage/>}/>
                     <Route path='/listprofiles' element={<ListProfiles/>}/>
                 </Routes>
             </BrowserRouter>
