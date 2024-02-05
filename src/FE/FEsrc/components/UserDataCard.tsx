8d879f9dd562d45e5303939e4afddbd250ac535b
Deep123845 <deep123845@gmail.com>
2024-02-05 15:59:19 -0500

    fix links and change color to match on cards

--- src/FE/FEsrc/components/UserDataCard.tsx
+++ src/FE/FEsrc/components/UserDataCard.tsx
@@ -1,32 +1,35 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 
-interface UserDataCardProps{
-    firstName: string;
-    lastName: string;
-    createdDate: string;
+interface UserDataCardProps {
+  firstName: string;
+  lastName: string;
+  createdDate: string;
 }
 
 
-const UserDataCard: React.FC<UserDataCardProps> = ({ firstName, lastName,  createdDate}) => {
+const UserDataCard: React.FC<UserDataCardProps> = ({ firstName, lastName, createdDate }) => {
   return (
-    <div className="cards" style= {cardStyle} >
-      <div className="name-circle" style= {circleStyle} >
+    <div className="cards" style={cardStyle} >
+      <div className="name-circle" style={circleStyle} >
         <span> Account Info </span>
       </div>
       <div className="card-content" style={contentStyle}>
         <div className="profile-name" style={nameStyle}>
-            First Name: {firstName}
+          First Name: {firstName}
         </div>
         <div className="profile-name" style={nameStyle}>
           Last Name: {lastName}
         </div>
         <div className="profile-name" style={nameStyle}>
-        Account Created: {createdDate}
+          Account Created: {createdDate}
         </div>
-        <a href="saved-trials.html" className= "saved-trials-link" >
-          Edit Account Info
-        </a>
+        <Link to="/savedTrials">
+          <div className="saved-trails-link">
+            Edit Account Info
+          </div>
+        </Link>
       </div>
     </div>
   );
@@ -36,39 +39,39 @@ export default UserDataCard;
 
 
 const cardStyle: React.CSSProperties = {
-    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
-    width: '360px',
-    margin: '10px',
-    textAlign: 'center',
-    fontFamily: 'arial',
-    borderRadius: '15px',
-    overflow: 'hidden',
-    flexDirection: 'column',
-    backgroundColor: 'rgba(255, 255, 255, 0.9)',
-  };
-  
-  const circleStyle: React.CSSProperties = {
-    backgroundColor: '#4CAF50',
-    color: 'white',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: '36px',
-    height: '90px',
-  };
-  
-  const contentStyle: React.CSSProperties = {
-    flex: 1,
-    padding: '20px',
-    textAlign: 'left',
-    overflow: 'hidden',
-  };
-  
-  const nameStyle: React.CSSProperties = {
-    fontSize: '18px',
-    height: '40px',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    whiteSpace: 'nowrap',
-    color: 'black'
-  };
+  boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
+  width: '360px',
+  margin: '10px',
+  textAlign: 'center',
+  fontFamily: 'arial',
+  borderRadius: '15px',
+  overflow: 'hidden',
+  flexDirection: 'column',
+  backgroundColor: 'rgba(255, 255, 255, 0.9)',
+};
+
+const circleStyle: React.CSSProperties = {
+  backgroundColor: '#039D5F',
+  color: 'white',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  fontSize: '36px',
+  height: '90px',
+};
+
+const contentStyle: React.CSSProperties = {
+  flex: 1,
+  padding: '20px',
+  textAlign: 'left',
+  overflow: 'hidden',
+};
+
+const nameStyle: React.CSSProperties = {
+  fontSize: '18px',
+  height: '40px',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap',
+  color: 'black'
+};
--- src/FE/FEsrc/components/UserProfileCard.tsx
+++ src/FE/FEsrc/components/UserProfileCard.tsx
@@ -1,23 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-interface UserProfileCardProps{
-    name: string;   
-    condition: string;
+interface UserProfileCardProps {
+  name: string;
+  condition: string;
 }
 
 const UserProfileCard: React.FC<UserProfileCardProps> = ({ name, condition }) => {
   return (
-    <div className="cards" style= {cardStyle} >
-      <div className="name-circle" style= {circleStyle} >
-        <span> { name } </span>
+    <div className="cards" style={cardStyle} >
+      <div className="name-circle" style={circleStyle} >
+        <span> {name} </span>
       </div>
       <div className="card-content" style={contentStyle}>
         <div className="profile-name" style={nameStyle}>
           Condition: {condition}
         </div>
-        <a href="saved-trials.html" className= "saved-trials-link" >
-          Edit Profile
-        </a>
+        <Link to="/savedTrials">
+          <div className="saved-trails-link">
+            Edit Profile
+          </div>
+        </Link>
       </div>
     </div>
   );
@@ -28,37 +31,37 @@ export default UserProfileCard;
 
 
 const cardStyle: React.CSSProperties = {
-    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
-    width: '300px',
-    margin: '10px',
-    textAlign: 'center',
-    fontFamily: 'arial',
-    borderRadius: '15px',
-    overflow: 'hidden',
-    flexDirection: 'column',
-    backgroundColor: 'rgba(255, 255, 255, 0.9)',
-  };
-  
-  const circleStyle: React.CSSProperties = {
-    backgroundColor: '#4CAF50',
-    color: 'white',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 8,
-    fontSize: '25px',
-    minHeight: 36,
-  };
-  
-  const contentStyle: React.CSSProperties = {
-    flex: 1,
-    padding: '20px',
-    textAlign: 'left',
-    overflow: 'hidden',
-  };
-  
-  const nameStyle: React.CSSProperties = {
-    fontSize: '18px',
-    paddingBottom: 15,
-    color: 'black'
-  };
+  boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
+  width: '300px',
+  margin: '10px',
+  textAlign: 'center',
+  fontFamily: 'arial',
+  borderRadius: '15px',
+  overflow: 'hidden',
+  flexDirection: 'column',
+  backgroundColor: 'rgba(255, 255, 255, 0.9)',
+};
+
+const circleStyle: React.CSSProperties = {
+  backgroundColor: '#039D5F',
+  color: 'white',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  padding: 8,
+  fontSize: '25px',
+  minHeight: 36,
+};
+
+const contentStyle: React.CSSProperties = {
+  flex: 1,
+  padding: '20px',
+  textAlign: 'left',
+  overflow: 'hidden',
+};
+
+const nameStyle: React.CSSProperties = {
+  fontSize: '18px',
+  paddingBottom: 15,
+  color: 'black'
+};
