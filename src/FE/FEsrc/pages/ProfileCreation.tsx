42bc4f411b238fc8e19556e2df93a1e1f64d095b
Deep123845 <deep123845@gmail.com>
2024-02-19 17:28:32 -0500

    added validation to profile creation page

--- src/FE/FEsrc/pages/ProfileCreation.tsx
+++ src/FE/FEsrc/pages/ProfileCreation.tsx
@@ -3,7 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import styled from '@emotion/styled';
 import { API_URL } from '..';
 import { conditions } from '../components/Constants';
-import { FormContainer, Form, FormLabel, TextInput, AutocompleteInput, ButtonContainer, FormButton, DropDownInput, AutocompleteTextField } from '../components/FormStyles';
+import { FormContainer, Form, FormLabel, TextInput, AutocompleteInput, ButtonContainer, FormButton, DropDownInput, AutocompleteTextField, FormButtonDisabled, ErrorMessage } from '../components/FormStyles';
+import { fieldValidation } from '../hooks/Validation';
 
 const ProfileCreationContainer = styled.div`
     min-width: fit-content;
@@ -29,14 +30,28 @@ const ProfileCreationPage = () => {
     });
 
     const [formValues, setFormValues] = useState({
-        name: '',
-        address: '',
-        dateOfBirth: '',
-        gender: '',
         user: userId,
         condition: '',
         advancedInfo: advancedInfoAsthma
     });
+
+    const checkEmpty = (value) => {
+        return value.trim() !== '';
+    };
+
+    const validateAddress = (value) => {
+        return value.split(',').length === 4;
+    };
+
+    const nameField = fieldValidation(checkEmpty);
+    const addressField = fieldValidation(validateAddress);
+    const dateOfBirthField = fieldValidation(checkEmpty);
+    const genderField = fieldValidation(checkEmpty);
+
+    const addressErrorMessage = 'Invalid address, please enter a valid address in the format: street, city, province, postal code';
+    const genericErrorMessage = 'This field cannot be empty';
+
+    const enableSubmit = nameField.valid && addressField.valid && dateOfBirthField.valid && genderField.valid;
 
     const [isHidden, setIsHidden] = useState({ asthma: true, COPD: true });
 
@@ -51,7 +66,7 @@ const ProfileCreationPage = () => {
         else {
             setIsHidden({ COPD: true, asthma: true });
         }
-    }
+    };
 
     const formatAddress = (address) => {
         const splitAddress = address.split(',');
@@ -61,24 +76,25 @@ const ProfileCreationPage = () => {
             province: splitAddress[2],
             postalCode: splitAddress[3]
         }
-    }
+    };
 
     const createRequestOptions = () => {
-        const formattedAddress = formatAddress(formValues.address);
+        const formattedAddress = formatAddress(addressField.value);
+        const mappedGender = genderMapping[genderField.value];
         return {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-                date_of_birth: formValues.dateOfBirth,
+                date_of_birth: dateOfBirthField.value,
                 user: formValues.user,
-                gender: genderMapping[formValues.gender],
-                title: formValues.name,
+                gender: mappedGender,
+                title: nameField.value,
                 condition: formValues.condition,
                 address: formattedAddress,
                 advanced_info: advancedInfoAsthma
             })
         };
-    }
+    };
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
@@ -95,7 +111,7 @@ const ProfileCreationPage = () => {
         } catch (error) {
             console.error('Error submitting form:', error);
         }
-    }
+    };
 
     useEffect(() => {
         handleAdvancedInfo();
@@ -110,48 +126,49 @@ const ProfileCreationPage = () => {
                         <TextInput
                             type='text'
                             id='name'
-                            value={formValues.name}
-                            onChange={(e) => {
-                                setFormValues({ ...formValues, name: e.target.value });
-                            }}
+                            value={nameField.value}
+                            onChange={nameField.handleChange}
+                            onBlur={nameField.handleBlur}
                         />
+                        {nameField.showErrorMessage && <ErrorMessage>{genericErrorMessage}</ErrorMessage>}
 
                         <FormLabel>Address</FormLabel>
                         <TextInput
                             type='text'
                             id='address'
-                            value={formValues.address}
-                            onChange={(e) => setFormValues({ ...formValues, address: e.target.value })}
+                            value={addressField.value}
+                            onChange={addressField.handleChange}
+                            onBlur={addressField.handleBlur}
                         />
+                        {addressField.showErrorMessage && <ErrorMessage>{addressErrorMessage}</ErrorMessage>}
 
                         <FormLabel>Date of Birth</FormLabel>
                         <TextInput
                             type='date'
                             id='date of birth'
-                            value={formValues.dateOfBirth}
-                            onChange={(e) => {
-                                setFormValues({ ...formValues, dateOfBirth: e.target.value })
-                            }
-                            }
+                            value={dateOfBirthField.value}
+                            onChange={dateOfBirthField.handleChange}
+                            onBlur={dateOfBirthField.handleBlur}
                         />
+                        {dateOfBirthField.showErrorMessage && <ErrorMessage>{genericErrorMessage}</ErrorMessage>}
 
                         <FormLabel>Gender</FormLabel>
                         <DropDownInput
-                            value={formValues.gender}
-                            onChange={(e) => setFormValues({ ...formValues, gender: e.target.value })}
+                            value={genderField.value}
+                            onChange={genderField.handleChange}
                         >
                             <option value='' disabled>-- Select Gender --</option>
                             <option value='Male'>Male</option>
                             <option value='Female'>Female</option>
                             <option value='Other'>Other</option>
                         </DropDownInput>
+                        {genderField.showErrorMessage && <ErrorMessage>{genericErrorMessage}</ErrorMessage>}
 
                         <FormLabel>Condition</FormLabel>
                         <AutocompleteInput
                             disablePortal
                             onInputChange={(event, value, reason) => {
                                 setFormValues({ ...formValues, condition: value })
-                                handleAdvancedInfo()
                             }}
                             freeSolo={true}
                             options={conditions}
@@ -159,7 +176,6 @@ const ProfileCreationPage = () => {
                                 onChange={(e) => {
                                     const newCondition = e.target.value;
                                     setFormValues({ ...formValues, condition: newCondition });
-                                    handleAdvancedInfo();
                                 }} />
                             }
                         />
@@ -236,7 +252,11 @@ const ProfileCreationPage = () => {
                             </>
                         )}
                         <ButtonContainer>
-                            <FormButton type='submit'>Save</FormButton>
+                            {enableSubmit ?
+                                <FormButton type='submit'>Save</FormButton>
+                                :
+                                <FormButtonDisabled disabled>Save</FormButtonDisabled>
+                            }
                         </ButtonContainer>
                     </Form>
                 </FormContainer>
