dcb93a8d220415cbbe9eb9a1cfbf1d31e8e650ef
Alan Scott <alansco.scott@gmail.com>
2024-02-20 16:48:15 -0500

    created separate file for filtering dictionaries, started integration

---
+++ src/backend/reach_backend/Api/filtering_dictionary.py
@@ -0,0 +1,21 @@
+'''File containing filtering dictionaries'''
+
+filtering_dict_num = {
+    "numExacerbations" : ["exacerbation", "Exacerbation"],
+    "numFlares" : ["flare", "flare up", "flare-up", "Flare", "Flare up", "Flare-up"],
+    "numCOPDFlares" : ["COPD flare", "COPD Flare"]
+}
+
+filtering_dict_boolean = {
+    "usesInhaler" : ["inhaler", "Inhaler"],
+    "usesInjection" : ["injection", "Injection"],
+    "isSmoker" : ["smoker", "smoking", "Smoker", "Smoking"],
+    "isEosinophilic" : ["Eosinophilic", "eosinophilic", "Eosinophilia", "eosinophilia"],
+    "usesPillsTablets" : ["pill", "Pill", "tablet", "Tablet", "oral", "Oral"],
+    "onDualTherapy" : ["dual therapy", "Dual therapy"],
+    "onTripleTherapy" : ["triple therapy", "Triple therapy"],
+}
+
+filtering_dict_special = {
+    
+}
--- src/backend/reach_backend/Api/trial_filterer.py
+++ src/backend/reach_backend/Api/trial_filterer.py
@@ -4,6 +4,8 @@ import regex as re
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
 from geopy.exc import GeopyError
+from .filtering_dictionary import (filtering_dict_num, 
+                                   filtering_dict_boolean, filtering_dict_special)
 
 locator = Nominatim(user_agent="my_request")
 
@@ -55,49 +57,23 @@ class TrialFilterer:
     @staticmethod
     def filter_keywords(df: pd.DataFrame, info: dict) -> pd.DataFrame:
         """increases rank for each met keyword"""
-        if info.get("numExacerbations", 0) > 0:
-            df.loc[
-                df["Keyword"].str.contains(
-                    "|".join(["exacerbation", "Exacerbation"]), na=False
+
+        for param in filtering_dict_num.keys():
+            if info.get(param, 0) > 0:
+                df.loc[df["Keyword"].str.contains(
+                    "|".join(filtering_dict_num.items()[param]), na=False
                 ),
                 ["KeywordRank"],
             ] += 1
-        if info.get("numFlares", 0) > 0:
-            df.loc[
-                df["Keyword"].str.contains(
-                    "|".join(
-                        [
-                            "flare",
-                            "flare up",
-                            "flare-up",
-                            "Flare",
-                            "Flare up",
-                            "Flare-up",
-                        ]
-                    ),
-                    na=False,
+
+        for param in filtering_dict_boolean.keys():
+            if info.get(param, 0):
+                df.loc[df["Keyword"].str.contains(
+                    "|".join(filtering_dict_num.items()[param]), na=False
                 ),
                 ["KeywordRank"],
             ] += 1
-        if info.get("usesInhaler"):
-            df.loc[
-                df["Keyword"].str.contains("|".join(["inhaler", "Inhaler"]), na=False),
-                ["KeywordRank"],
-            ] += 1
-        if info.get("usesInjection"):
-            df.loc[
-                df["Keyword"].str.contains(
-                    "|".join(["injection", "Injection"]), na=False
-                ),
-                ["KeywordRank"],
-            ] += 1
-        if info.get("isSmoker"):
-            df.loc[
-                df["Keyword"].str.contains(
-                    "|".join(["smoker", "smoking", "Smoker", "Smoking"]), na=False
-                ),
-                ["KeywordRank"],
-            ] += 1
+
         if info.get("asthmaSeverity") in ["moderate", "severe"]:
             df.loc[
                 df["Keyword"].str.contains(
@@ -113,45 +89,7 @@ class TrialFilterer:
                 ),
                 ["KeywordRank"],
             ] += 1
-        if info.get("isEosinophilic"):
-            df.loc[
-                df["Keyword"].str.contains(
-                    "|".join(
-                        ["Eosinophilic", "eosinophilic", "Eosinophilia", "eosinophilia"]
-                    ),
-                    na=False,
-                ),
-                ["KeywordRank"],
-            ] += 1
-        if info.get("usesPillsTablets"):
-            df.loc[
-                df["Keyword"].str.contains(
-                    "|".join(["pill", "Pill", "tablet", "Tablet", "oral", "Oral"]),
-                    na=False,
-                ),
-                ["KeywordRank"],
-            ] += 1
-        if info.get("onDualTherapy"):
-            df.loc[
-                df["Keyword"].str.contains(
-                    "|".join(["dual therapy", "Dual therapy"]), na=False
-                ),
-                ["KeywordRank"],
-            ] += 1
-        if info.get("onTripleTherapy"):
-            df.loc[
-                df["Keyword"].str.contains(
-                    "|".join(["triple therapy", "Triple therapy"]), na=False
-                ),
-                ["KeywordRank"],
-            ] += 1
-        if info.get("numCOPDFlares", 0) > 0:
-            df.loc[
-                df["Keyword"].str.contains(
-                    "|".join(["COPD flare", "COPD Flare"]), na=False
-                ),
-                ["KeywordRank"],
-            ] += 1
+            
         return df
 
     @staticmethod
