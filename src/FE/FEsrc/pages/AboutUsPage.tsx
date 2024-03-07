6a60a28ab84cd8347199a06692c829df03ca6f79
davimang <88117511+davimang@users.noreply.github.com>
2024-03-07 14:55:57 -0500

    Update faq contact about (#191)
    * Add about us
    
    * Update faq + contact + about us

--- src/FE/FEsrc/pages/AboutUsPage.tsx
+++ src/FE/FEsrc/pages/AboutUsPage.tsx
@@ -22,15 +22,21 @@ const AboutUsPage = () => {
         <AboutUsContainer>
             <AboutUsText>
                 <h1>Motivation behind REACH</h1>
-                <p>Reach was developed by a team of final year Software Engineering students
-                    as part of an eight month long capstone course. Additionally, the project was supervised (and proposed) by
-                    two respirolagists from Mcmaster, Dr. Terrence Ho & Dr. Ciaran Scallan. It was created
-                    with the ultimate goal of providing both clincians & patients easier access to clincial trials from
-                    around the world.
+                <p>At their best, clinical trials and other forms of research provide 
+                    an opportunity for patients to access treatments or tests that are not 
+                    available, which can lead to improved health for patients. Unfortunately, 
+                    getting patients into studies even if they are motivated is limited by 
+                    difficulties in accessing information about studies and suboptimal 
+                    connectivity between research teams, healthcare providers, and patients. 
                 </p>
-                <h1>The Engineering Team</h1>
-                <p>The team consists of Aamina Hussain, Alan Scott, Anika Peer, David Moroniti, and Deep Raj. If you would
-                    like to contact one of the members, please see our "Contact Us" page.
+                <p>REACH was developed by a team of final year Software Engineering students as part 
+                    of an 8-month long course, who were supervised by two Respirologists - Drs. Terence 
+                    Ho and Ciaran Scallan from McMaster University. It was created to make it easier for 
+                    clinicians and patients to “match” a patient to a study, using a database of active 
+                    research studies. In its current form, REACH is only able to do this in the realm of 
+                    Respiratory Medicine, including asthma, chronic obstructive pulmonary disease (COPD), 
+                    interstitial lung disease (ILD), pulmonary hypertension, chronic cough, and sleep-disordered 
+                    breathing (e.g. obstructive sleep apnea). 
                 </p>
             </AboutUsText>
         </AboutUsContainer>
