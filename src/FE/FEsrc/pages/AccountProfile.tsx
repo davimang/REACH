83a010293d3bfa556cb14f30db2e79945b6d505e
A Peer <47980558+AKP101@users.noreply.github.com>
2024-03-11 03:02:05 -0400

    Needs to be styled

--- src/FE/FEsrc/pages/AccountProfile.tsx
+++ src/FE/FEsrc/pages/AccountProfile.tsx
@@ -1,65 +1,58 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'; 
 import { API_URL } from '..';
-import UserDataCard from '../components/UserDataCard';
 import styled from '@emotion/styled';
-import { Button } from '@mui/material';
-import { StyledButton } from '../components/ButtonStyle';
-import { UserData } from '../components/types';
 
 const AccountProfilePageContainer = styled.div`
     display: flex;
 `;
 
 const Header = styled.h1`
-    color: white
+    color: white;
 `;
 
-const ProfileListContainer = styled.div`
-  display: grid;
-  justify-content: center;
-  width: 45%;
+const AccountInfoContainer = styled.div`
+    padding: 25px;
+    width: 45%;
+    height: fit-content;
+    justify-content: center;
+    display: grid;
+    color: #FFFFFF;
+    font-size: 20px;
+    font-family: math;
 `;
 
-const UserDataContainer = styled.div`
-  padding: 25px;
-  width: 45%;
-  height: fit-content;
-  justify-content: center;
-  display: grid;
-  color: #FFFFFF;
-  font-size: 20px;
-  font-family: math;
+const EditButton = styled.button`
+    background-color: #4CAF50;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+    margin-top: 20px;
 `;
 
-const SizedButton = styled(StyledButton)`
-    height: 65px;
-    width: inherit;
-    margin-left: 4em;
-`;
+interface UserData {
+  first_name: string;
+  last_name: string;
+  email: string;
+  clinician_account: boolean;
+}
 
-const AccountProfilePage: React.FC = () => {
-  const userId = localStorage.getItem('userId');
-  const [userData, setUserData] = useState<UserData>({ first_name: "", last_name: "", created: "", is_clinician: false });
-  const [authToken, setAuthToken] = useState(localStorage.getItem('accessToken'));
+const AccountProfile: React.FC = () => {
+
+  const userId = localStorage.getItem('userId') ? localStorage.getItem('userId') : 1;
+  const [userData, setUserData] = useState<UserData>({ first_name: '', last_name: '', email: '', clinician_account: false });
 
   const fetchUserData = () => {
     try {
       const endpoint = `/userdata/${userId}/`;
-      const requestOptions = {
-        headers: { 'Authorization': `Bearer ${authToken}` }
-      };
-      fetch(`${API_URL}${endpoint}`, requestOptions)
-        .then(response => response.json())
-        .then(response => { setUserData(response) });
+      fetch(`${API_URL}${endpoint}`).then(response => response.json()).then(response => { setUserData(response) });
     } catch (error) {
       console.error('Error fetching account info:', error.message);
     }
   }
-
-  useEffect(() => {
-    setAuthToken(localStorage.getItem('accessToken'));
-  }, [localStorage.getItem('accessToken')]);
 
   useEffect(() => {
     fetchUserData();
@@ -67,18 +60,17 @@ const AccountProfilePage: React.FC = () => {
 
   return (
     <AccountProfilePageContainer>
-      <UserDataContainer>
-        <UserDataCard userData={userData} />
-        <Link to={`/editProfile`}> {/* Link to the edit profile page */}
-          <SizedButton>Edit Profile</SizedButton>
+      <AccountInfoContainer>
+        <Header>Account Information</Header>
+        <p>Name: {`${userData.first_name} ${userData.last_name}`}</p>
+        <p>Email: {`${userData.email}`}</p>
+        <p>Clinician Account: {userData.clinician_account ? 'Yes' : 'No'}</p>
+        <Link to="/editAccount"> 
+          <EditButton>Edit Account Information</EditButton>
         </Link>
-      </UserDataContainer>
-      <ProfileListContainer>
-        <Header>Profiles</Header>
-        {/* Add a button here to add a new profile if needed */}
-      </ProfileListContainer>
+      </AccountInfoContainer>
     </AccountProfilePageContainer>
   );
 };
 
-export default AccountProfilePage;
+export default AccountProfile;
