45a9b7c911cad50f6a5749e42dd063027529994e
A Peer <47980558+AKP101@users.noreply.github.com>
2024-03-16 13:24:17 -0400

    Fix awkward wording

--- src/FE/FEsrc/components/TrialModal.tsx
+++ src/FE/FEsrc/components/TrialModal.tsx
@@ -46,14 +46,14 @@ const TrialModal: React.FC<TrialModalProps> = ({ open, handleModal, modalDetails
 
     const emailTemplate = !isClinician ? `Dear ${contact},
 
-My name is ${usersName}. I found your study on ${modalDetails["url"]} for patients with ${condition} through REACH, an app which helps match patients 
+My name is ${usersName}. I found your study on ${modalDetails["url"]} for patients with ${condition} through REACH, an app that helps match patients 
 to research studies, and I am interested in participating.
 Please let me know how I could participate in your study.
 
 Thank you,`: `Dear ${contact},
 
-My name is Dr. ${usersName}. I am looking for clincial studies on behalf of one of my patients, and through REACH, an app which helps match patients to research
-studies, I found your study on ${modalDetails["url"]} for patients with ${condition} and I am interested in having them participate.
+My name is Dr. ${usersName}. I am looking for clincial studies on behalf of one of my patients through REACH, an app that helps match patients to research
+studies. I found your study on ${modalDetails["url"]} for patients with ${condition} and I am interested in having my patient participate.
 Please let me know how I could set them up to participate in your study.
 
 Thank you,`;
