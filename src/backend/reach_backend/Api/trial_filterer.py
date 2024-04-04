8e60f0585c0ab75c8a3fd4d323e9f02dd7a4581e
Alan Scott <alansco.scott@gmail.com>
2024-04-03 21:05:45 -0400

    added comments to trial_filterer

--- src/backend/reach_backend/Api/trial_filterer.py
+++ src/backend/reach_backend/Api/trial_filterer.py
@@ -21,13 +21,14 @@ class TrialFilterer:
         """filters trials based on input criteria"""
         age = input_params["age"]
 
-        # convert min, max ages, filter out ineligible
+        # convert min, max ages to numeric values, filter out ineligible trials
         studies["MinimumAge"] = studies["MinimumAge"].apply(TrialFilterer.clean_age)
         studies["MaximumAge"] = studies["MaximumAge"].apply(TrialFilterer.clean_age)
         studies = studies[
             (age >= studies["MinimumAge"]) & (age <= studies["MaximumAge"])
         ]
 
+        # filter out trials if they aren't applicable based on sex
         studies = TrialFilterer.filter_gender(input_params["sex"], studies)
         return studies
 
@@ -43,29 +44,33 @@ class TrialFilterer:
             if k in input_params.keys() and input_params.get(k, 0):
                 search_str.append(filtering_dict_boolean.get(k, 0))
 
-        # hard coded special variables
+        # add keyword to search string based on asthma severity
         if input_params.get("asthmaSeverity", "") == "moderate":
             search_str.append("moderate+asthma")
         elif input_params.get("asthmaSeverity", "") == "severe":
             search_str.append("severe+asthma")
 
+        # add keyword to search string based on WHO class
         if input_params.get("WHOFunctionalClass", "0") in ["3", "4"]:
             search_str.append("severe+pulmonary+hypertension+OR+severe+PH")
 
-        packYears = (
+        # calculate pack-years
+        pack_years = (
             input_params.get("packYears", 0)
             or input_params.get("dailySmokes", 0)
             * input_params.get("yearsSmoked", 0)
             / 20
         )
 
-        if packYears >= 40:
+        # add smoking severity to search string based on pack-years
+        if pack_years >= 40:
             search_str.append("heavy+smoker+OR+heavy+smoking")
-        elif packYears >= 20:
+        elif pack_years >= 20:
             search_str.append("moderate+smoker+OR+moderate+smoking")
-        elif packYears > 0:
+        elif pack_years > 0:
             search_str.append("light+smoker+OR+light+smoking")
 
+        # add sleep keyword to search string based on severity
         if input_params.get("apneadIndex", 0) >= 30:
             search_str.append("severe+sleep+apnea")
         elif input_params.get("apneadIndex", 0) >= 15:
@@ -73,9 +78,11 @@ class TrialFilterer:
         elif input_params.get("apneadIndex", 0) >= 5:
             search_str.append("mild+sleep+apnea")
 
+        # oxygen saturation keywording
         if input_params.get("nadirO2Saturation", 1) <= 0.9:
             search_str.append("low+oxygen+saturation")
 
+        # therapy keywords
         if input_params.get("backgroundTherapy", "") in [
             "dual therapy",
             "triple therapy",
@@ -90,6 +97,7 @@ class TrialFilterer:
                 case "triple therapy":
                     search_str.append("triple+therapy")
 
+        # add keywords based on mask type
         if input_params.get("maskType", "") in ["nasal", "oronasal", "full face"]:
             match input_params.get("backgroundTherapy", ""):
                 case "nasal":
@@ -105,7 +113,10 @@ class TrialFilterer:
                 search_str.append("BiPAP-S")
             else:
                 search_str.append("BiPAP-ST")
-        # FEV healthy range calculation
+
+        # FEV1/FEV% healthy range calculation
+        # should be within the healthy range based on sex
+        # dict[i][j] -> i is male/female, j is upper/lower bound
         if input_params.get("FEV", -1) > 0:
             if input_params.get("sex", "").lower() == "male":
                 if (
@@ -142,6 +153,9 @@ class TrialFilterer:
             ):
                 search_str.append("high+FEV+ratio+OR+high+FEV1+FVC+ratio")
 
+        # FVC/FVC% healthy range calculation
+        # should be within the healthy range based on sex
+        # dict[i][j] -> i is male/female, j is upper/lower bound
         if input_params.get("FVC", -1) > 0:
             if input_params.get("sex", "").lower() == "male":
                 if (
@@ -166,9 +180,11 @@ class TrialFilterer:
                 ):
                     search_str.append("high+FVC+OR+high+FVC")
 
+        # keyword based on lung diffusion
         if input_params.get("DLCO", 1) < filtering_dict_special.get("DLCO", [0.75])[0]:
             search_str.append("low+lung+diffusion+OR+low+DLCO")
 
+        # keyword based on blood eosinophils
         if (
             input_params.get("bloodEosinophil", 300)
             < filtering_dict_special.get("bloodEosinophil", [30, 350])[0]
@@ -181,6 +197,7 @@ class TrialFilterer:
         ):
             search_str.append("high+blood+eosinophil+count+OR+high+eosinophil")
 
+        # keyword based on BMI
         if (
             input_params.get("BMI", 20)
             < filtering_dict_special.get("BMI", [18.5, 25])[0]
@@ -193,12 +210,14 @@ class TrialFilterer:
         ):
             search_str.append("overweight+OR+over+weight+OR+high+BMI")
 
+        # keyword based on cough severity
         if (
             input_params.get("coughSeverity") is not None
             and input_params.get("coughSeverity", 0) > 4
         ):
             search_str.append("severe+cough+OR+heavy+cough+OR+VAS+40mm")
 
+        # return formatted string if there are search strings compiled
         if len(search_str) > 0:
             return "+OR+".join(search_str)
         return ""
@@ -209,10 +228,13 @@ class TrialFilterer:
     ) -> pd.DataFrame:
         """creates full address and populates distance"""
 
+        # generate user address
         studies = TrialFilterer.generate_address(studies)
 
+        # set default distance to -1
         studies["Distance"] = -1
 
+        # calculate geodesic distance between user address and trial location
         for i in studies.index:
             studies.at[i, "Distance"] = round(
                 TrialFilterer.get_distance_km(
@@ -224,7 +246,8 @@ class TrialFilterer:
                 2,
             )
 
-        max_distance = input_params.get("max_distance", 999999999)  # default 500 km
+        # remove studies outside of the max range
+        max_distance = input_params.get("max_distance", 999999999)
         studies = studies[studies["Distance"] <= max_distance]
         return studies
 
@@ -232,7 +255,10 @@ class TrialFilterer:
     def generate_address(studies: pd.DataFrame) -> pd.DataFrame:
         """generates the address of the trial through the given fields"""
         studies["FullAddress"] = ""
+
+        # for each field, append the location/city/state/zip if it exists
         for i in studies.index:
+            # append facility location
             studies.at[i, "FullAddress"] = (
                 studies.at[i, "LocationFacility"] + ", "
                 if (
@@ -241,6 +267,7 @@ class TrialFilterer:
                 )
                 else studies.at[i, "FullAddress"]
             )
+            # append city
             studies.at[i, "FullAddress"] = (
                 studies.at[i, "FullAddress"] + studies.at[i, "LocationCity"]
                 if (
@@ -249,6 +276,7 @@ class TrialFilterer:
                 )
                 else studies.at[i, "FullAddress"]
             )
+            # append state/province
             studies.at[i, "FullAddress"] = (
                 studies.at[i, "FullAddress"] + ", " + studies.at[i, "LocationState"]
                 if (
@@ -257,6 +285,7 @@ class TrialFilterer:
                 )
                 else studies.at[i, "FullAddress"]
             )
+            # append zip/postal code
             studies.at[i, "FullAddress"] = (
                 studies.at[i, "FullAddress"] + " " + str(studies.at[i, "LocationZip"])
                 if (
@@ -265,6 +294,7 @@ class TrialFilterer:
                 )
                 else studies.at[i, "FullAddress"]
             )
+            # append country
             studies.at[i, "FullAddress"] = (
                 studies.at[i, "FullAddress"] + ", " + studies.at[i, "LocationCountry"]
                 if (
@@ -273,6 +303,7 @@ class TrialFilterer:
                 )
                 else studies.at[i, "FullAddress"]
             )
+            # replace characters to prevent frontend issues with Google Maps API
             studies.at[i, "FullAddress"] = studies.at[i, "FullAddress"].replace(
                 " & ", " and "
             )
@@ -292,7 +323,6 @@ class TrialFilterer:
         """calculates distance between two addresses"""
         return geodesic((lat1, long1), (lat2, long2)).kilometers
 
-    # convert min and max ages to float type
     @staticmethod
     def clean_age(age: float) -> int:
         """cleans up user age input"""
